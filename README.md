8009dac239a4bb80c887dffaa11200a24c4ef93b
nsjux <ns2jux@gmail.com>
2024-10-15 08:59:41 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### Django Notion Page
+✅https://www.notion.so/dji00/django-10aa108359b480aabb58d5c2f2122aab?pvs=4
+### 최근 업로드
+😊24-10-11: templates
---
+++ mysite/home/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import (HttpResponse,HttpResponseRedirect)
+from django.urls import reverse
+
+def index(request):
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponseRedirect(
+    #     reverse('detail', args=[1]))
+    # return HttpResponseRedirect(
+    #     reverse('detail', kwargs={'question_id': 1}))
+    ctx = {
+        "greetings": "Hello there!",
+        "location": {
+            "city": "Seoul",
+            "country": "South Korea"
+        },
+        "languages": ["Korean", "English"]
+    }
+    return render(request, 'home/index.html', context=ctx)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
