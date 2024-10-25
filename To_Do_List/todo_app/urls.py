7b65e15eb66c250c1538a5b2982d9629b6f7f90f
nsjux <ns2jux@gmail.com>
2024-10-25 13:37:24 +0900

    todolist_ver1

--- To_Do_List/todo_app/urls.py
+++ To_Do_List/todo_app/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name='todo_app'
+
 urlpatterns = [
-    path('',views.test),
+    path('',views.TestPageView.as_view(), name='testpage'),
+    path('todolist/',views.TodoListView.as_view(), name='todolist'),
+    path('createtodo/',views.TodoCreateView.as_view(), name='todo-create'),
+
+    # path('complit_todo/',views.TodoComplitView.as_view(), name='todo-complit'),
+    # path('',TestPageView.as_view(), name='testpage'),
+    
 ]
--- To_Do_List/todo_app/views.py
+++ To_Do_List/todo_app/views.py
@@ -1,5 +1,41 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.utils import timezone
+from django.urls import reverse_lazy
 
-# Create your views here.
-def test(request):
-    return render(request,"test.html")
+from todo_app.forms import Todoform
+from todo_app.models import Todo
+
+
+class TestPageView(TemplateView):
+    template_name = "todo_app/test.html"
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context["latest_articles"] = Article.objects.all()[:5]
+    #     return context
+
+class TodoListView(ListView):
+    model = Todo
+    # queryset = Todo.objects.filter(complit__iexact='0')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ['todo_list','complit']
+    
+    success_url = reverse_lazy("todo_app:testpage")
+    def form_valid(self, form):
+        form.instance.date = timezone.now().date() 
+        return super().form_valid(form)
+
+# class TodoComplitView():
+ 
+
