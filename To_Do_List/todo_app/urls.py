5c7de95e5686917104a96dd2e7f78e8b42fdb29e
Jiyeong <ns2jux@gmail.com>
2024-10-28 16:07:58 +0900

    todolist_test

--- To_Do_List/todo_app/urls.py
+++ To_Do_List/todo_app/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('',views.TestPageView.as_view(), name='testpage'),
     path('todolist/',views.TodoListView.as_view(), name='todolist'),
     path('createtodo/',views.TodoCreateView.as_view(), name='todo-create'),
+    path('detail_todo/<int:pk>',views.TodoDetailView.as_view(), name='todo-detail'),
+    path('update/<int:pk>',views.TodoUpdateView.as_view(), name='todo-update'),
+    path('delete/<int:pk>',views.TodoDeleteView.as_view(),name='todo-delete')
 
     # path('complit_todo/',views.TodoComplitView.as_view(), name='todo-complit'),
     # path('',TestPageView.as_view(), name='testpage'),
